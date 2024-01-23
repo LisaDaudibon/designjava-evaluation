fa0b48d5eec57b413811286a26a52f5a06bd8039
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-23 13:37:45 +0100

    :sparkles: LD initializing the class Hero with the constructor, getter and method takeDamages

--- src/main/java/com/zenika/Hero.java
+++ src/main/java/com/zenika/Hero.java
@@ -1,4 +1,35 @@
 package com.zenika;
 
 public class Hero {
+    private String name;
+    private String originStory;
+    private Weapon heroWeapon;
+    private int pv;
+
+    public Hero(String name, String originStory) {
+        this.name = name;
+        this.originStory = originStory;
+        this.heroWeapon = null;
+        this.pv = 100;
+    }
+
+    public String getOriginStory() {
+        return originStory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Weapon equipWeapon( Weapon weapon) {
+        return heroWeapon = weapon;
+    }
+
+    public Weapon getWeapon(){
+        return heroWeapon;
+    }
+
+    public int takeDamages ( int pv, Weapon heroWeapon) {
+        return Math.subtractExact(pv, heroWeapon.getMaxDamages());
+    }
 }
