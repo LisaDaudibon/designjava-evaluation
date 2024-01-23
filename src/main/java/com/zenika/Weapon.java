32ac18e0cdc2dc6a96575311007a8741f3b03e99
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-23 13:35:26 +0100

    :sparkles: LD initializing the class Weapon

--- src/main/java/com/zenika/Weapon.java
+++ src/main/java/com/zenika/Weapon.java
@@ -1,4 +1,18 @@
 package com.zenika;
 
 public class Weapon {
+    private String name;
+    private int maxDamages;
+    private Weapon (String name, int maxDamages){
+        this.name = name;
+        this.maxDamages = maxDamages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getMaxDamages() {
+        return maxDamages;
+    }
 }
