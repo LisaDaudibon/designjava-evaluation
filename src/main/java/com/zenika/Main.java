ba23af46c23805c74f16bcfcd58d1849f08ede03
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-23 14:35:10 +0100

    :sparkles: adding the class Story and its supercharge of the method toString, implementation of it all into the class Main

--- src/main/java/com/zenika/Main.java
+++ src/main/java/com/zenika/Main.java
@@ -2,6 +2,15 @@ package com.zenika;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hero balourd = new Hero( "Balourd",
+                "Ce petit ourson aimait se prendre pour un pirate de l'espace. \n" +
+                        "Chaque nuit, il partait dans ses rêves visitait de nouvelles planètes à bord de son magnifique vaisseau spatial.");
+        Weapon pioche = new Weapon("une pioche", 25 );
+        Place foretEnchantee = new Place("une forêt enchantée", "ce lieu bourdonné de fées et korrigans qui jouaient joyeusement tous ensemble.", pioche);
+        Enemy alfred = new Enemy("Alfred", "Je te cherche depuis bien longtemps et je t'ai enfin trouvé !", 10);
+
+        Story bestStoryEver = new Story(balourd, foretEnchantee, alfred);
+        bestStoryEver.setTheStorysWeapon(balourd);
+        System.out.println(bestStoryEver);
     }
 }
---
+++ src/main/java/com/zenika/Story.java
@@ -0,0 +1,46 @@
+package com.zenika;
+
+public class Story {
+    private Hero theStorysHero;
+    public Weapon theStorysWeapon;
+    private Place thePlaceOfTheStory;
+    private Enemy theStorysEnemy;
+    public Story (Hero theStorysHero, Place thePlaceOfTheStory, Enemy theStorysEnemy) {
+        this.theStorysHero = theStorysHero;
+        this.theStorysWeapon = thePlaceOfTheStory.getLocalWeapon();
+        this.thePlaceOfTheStory = thePlaceOfTheStory;
+        this.theStorysEnemy = theStorysEnemy;
+    }
+
+    public Weapon setTheStorysWeapon(Hero theStorysHero) {
+        return theStorysWeapon = theStorysHero.equipWeapon(thePlaceOfTheStory.getLocalWeapon());
+    }
+
+    @Override
+    public String toString (){
+        return String.format("""
+                ✨ Il était une fois, l'histoire d'un héros. ✨
+                
+                🧙🏼‍♂️ Le héros de cette histoire est %s.
+                %s
+                
+                📜 Un jour notre héros %s, partit à l'aventure dans %s, 
+                %s
+                Il trouva alors %s.
+                
+                😈 Peu de temps après, il croisa alors le chemin de %s.
+                %s lui dit alors : %s
+                
+                ⚔️ Le combat était maintenant inévitable...
+                %s : %s PV
+                %s : %s dommage max/tour
+                vs
+                %s : %s PV
+                %s tours pour invoquer son attaque ultime !
+                """, theStorysHero.getName(), theStorysHero.getOriginStory(), theStorysHero.getName(),
+                thePlaceOfTheStory.getName(), thePlaceOfTheStory.getDescription(), thePlaceOfTheStory.getLocalWeapon().getName(),
+                theStorysEnemy.getName(), theStorysEnemy.getName(), theStorysEnemy.getProvocationSentence(),
+                theStorysHero.getName(), theStorysHero.getHealth(), theStorysHero.getWeapon().getName(), theStorysHero.getWeapon().getMaxDamages(),
+                theStorysEnemy.getName(), theStorysEnemy.getHealth(), theStorysEnemy.getCastTime());
+    }
+}
