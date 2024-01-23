cbeba3cf502e3f0cf0dab5e80f49f7b096bcfd30
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-23 13:49:05 +0100

    :sparkles: LD initializing my class Place with constructor and getter

--- src/main/java/com/zenika/Place.java
+++ src/main/java/com/zenika/Place.java
@@ -1,4 +1,24 @@
 package com.zenika;
 
 public class Place {
+    private String name;
+    private String description;
+    private Weapon localWeapon;
+    public Place (String name, String description, Weapon localWeapon) {
+        this.name = name;
+        this.description = description;
+        this.localWeapon = localWeapon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Weapon getLocalWeapon() {
+        return localWeapon;
+    }
 }
